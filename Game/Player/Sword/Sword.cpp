4d3293f72e7a21951d21c044cb2fa625313ee980
fujitsukaharuto <146906715+fujitsukaharuto@users.noreply.github.com>
2026-06-29 16:54:23 +0900

    code Fix

--- Game/Player/Sword/Sword.cpp
+++ Game/Player/Sword/Sword.cpp
@@ -19,44 +19,26 @@ void Sword::ConfigureAsAttackHitbox(
 	SetCastShadow(false);
 	SetTransient(true);
 
-	switch (owner) {
-	case Sword::HitboxOwner::Player:
-		SetName("PlayerAttackHitbox");
-		InitializeCollider(ColliderKind::Box);
-		if (auto* collider = GetCollider()) {
-			ColliderConfig config;
-			config.isCollisionEnabled = true;
-			config.isTrigger = true;
-			config.isDraw = drawCollider;
-			config.colliderType = static_cast<int>(ColliderType::Type_PlayerAttack);
-			config.targetType = static_cast<int>(ColliderType::Type_Enemy);
-			config.size = size;
-			collider->ApplyConfig(config);
-			collider->SetName("PlayerAttackHitboxCollider");
-			if (auto* box = dynamic_cast<BoxCollider*>(collider)) {
-				box->SetSize(size);
-			}
-		}
-		break;
-	case Sword::HitboxOwner::Enemy:
-		SetName("EnemyAttackHitbox");
-		InitializeCollider(ColliderKind::Box);
-		if (auto* collider = GetCollider()) {
-			ColliderConfig config;
-			config.isCollisionEnabled = true;
-			config.isTrigger = true;
-			config.isDraw = drawCollider;
-			config.colliderType = static_cast<int>(ColliderType::Type_EnemyAttack);
-			config.targetType = static_cast<int>(ColliderType::Type_Player);
-			config.size = size;
-			collider->ApplyConfig(config);
-			collider->SetName("EnemyAttackHitboxCollider");
-			if (auto* box = dynamic_cast<BoxCollider*>(collider)) {
-				box->SetSize(size);
-			}
-		}
-		break;
-	default:
-		break;
-	}
+    const bool isPlayer = (owner == HitboxOwner::Player);
+    const char* objectName = isPlayer ? "PlayerAttackHitbox" : "EnemyAttackHitbox";
+    const char* colliderName = isPlayer ? "PlayerAttackHitboxCollider" : "EnemyAttackHitboxCollider";
+    const ColliderType selfType = isPlayer ? ColliderType::Type_PlayerAttack : ColliderType::Type_EnemyAttack;
+    const ColliderType targetType = isPlayer ? ColliderType::Type_Enemy : ColliderType::Type_Player;
+
+    SetName(objectName);
+    InitializeCollider(ColliderKind::Box);
+    if (auto* collider = GetCollider()) {
+        ColliderConfig config;
+        config.isCollisionEnabled = true;
+        config.isTrigger = true;
+        config.isDraw = drawCollider;
+        config.colliderType = static_cast<int>(selfType);
+        config.targetType = static_cast<int>(targetType);
+        config.size = size;
+        collider->ApplyConfig(config);
+        collider->SetName(colliderName);
+        if (auto* box = dynamic_cast<BoxCollider*>(collider)) {
+            box->SetSize(size);
+        }
+    }
 }
